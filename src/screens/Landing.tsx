20ab2f9e4951f884dd1cca3b381e719ff1205a19
mitulagr2 <mitulagr2@gmail.com>
2024-02-04 15:44:47 +0530

    add model

---
+++ src/screens/Landing.tsx
@@ -0,0 +1,12 @@
+import styles from "./Landing.module.css";
+
+const Landing = () => {
+  console.log("landing");
+  return (
+    <div className={styles.Coffee_Shop_Model}>
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default Landing;
