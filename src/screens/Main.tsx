8d20b57e76b8baed7a2a67142f3af3c06ca0de98
mitulagr2 <mitulagr2@gmail.com>
2024-02-04 21:18:49 +0530

    layout v2

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,13 +1,13 @@
 import styles from "./Main.module.css";
 import InfoSection from "../components/InfoSection/InfoSection";
-import MenuSection from "../components/MenuSection/MenuSection";
+// import MenuSection from "../components/MenuSection/MenuSection";
 
 const Main = () => {
   console.log("main");
   return (
     <div className={styles.MainWrapper}>
       <InfoSection />
-      <MenuSection />
+      {/* <MenuSection /> */}
     </div>
   );
 };
